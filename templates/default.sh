7dfb4d3e06d2f27d7d84c900c49f1750f13f2689
Marcin Polak <mapoart@gmail.com>
2020-01-28 18:31:17 +0100

    cleanups

--- templates/default.sh
+++ templates/default.sh
@@ -1,22 +1,16 @@
 # Nexss PROGRAMMER 2.0.0 - Bash
 # Default template for JSON Data
 # This works also on Windows (WSL - Windows Subsystem Linux)
-
 # !Important
 # Remeber to change CRLF to LF (Windows to Unix)
 # In VSCode you can mae it from bottom status bar
 # Click on CRLF and change it to LF.
-
 # For JSON in bash is good to use
 # https://stedolan.github.io/jq/download/
-
 # STDIN
-
 NexssStdin=`cat`
 # Modify Data
 # NexssStdout=$(echo "$NexssStdin"|jq ".outputBash = \"Hello from Bash! $BASH_VERSION\"")
-
 NexssStdout=$(echo "$NexssStdin"|jq ".test = \"test\"")
-
 # STDOUT
 echo $NexssStdout>&1
--- templates/helloWorld.sh
+++ templates/helloWorld.sh
@@ -1,20 +1,15 @@
 # Nexss PROGRAMMER 2.0.0 - Bash
 # Default template for JSON Data
 # This works also on Windows (WSL - Windows Subsystem Linux)
-
 # !Important
 # Remeber to change CRLF to LF (Windows to Unix)
 # In VSCode you can mae it from bottom status bar
 # Click on CRLF and change it to LF.
-
 # For JSON in bash is good to use
 # https://stedolan.github.io/jq/download/
-
 # STDIN
-
 NexssStdin=`cat`
 # Modify Data
 NexssStdout=$(echo "$NexssStdin"|jq ".HelloFromBash = \"$BASH_VERSION\"")
-
 # STDOUT
 echo $NexssStdout>&1
