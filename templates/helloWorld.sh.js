0898473323563db3210fb813eb6cb2851f9ac181
Marcin Polak <mapoart@gmail.com>
2020-09-12 19:22:42 +0200

    Added installation of jq for all distributions

--- templates/helloWorld.sh.js
+++ templates/helloWorld.sh.js
@@ -1,9 +1,24 @@
 // additional info for templates like copy extra libraries.
+
+const { platform } = require("os");
+
+let config;
 // in this case library needs JSON
-const config = {
-  files: [],
-  commands: ["scoop install jq", "scoop update jq"],
-  repos: []
-};
+if (process.platform === "win32") {
+  config = {
+    files: [],
+    commands: ["scoop install jq", "scoop update jq"],
+    repos: [],
+  };
+} else {
+  const {
+    replaceCommandByDist,
+  } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+  config = {
+    files: [],
+    commands: [replaceCommandByDist("apt-get install -y jq")],
+    repos: [],
+  };
+}
 
 module.exports = config;
